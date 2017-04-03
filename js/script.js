64fdd9bf91daca042d584e6d1a14991afd0373a2
Roma <de11romik@gmail.com>
2017-04-03 14:43:06 +0000

    mob

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+var slideout = new Slideout({
+  'panel': document.getElementById('main'),
+  'menu': document.getElementById('menu'),
+  'padding': 256,
+  'tolerance': 70
+});
+
+document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
+  slideout.open();
+});
+
+function close(eve) {
+  eve.preventDefault();
+  slideout.close();
+}
+
+slideout
+  .on('beforeopen', function() {
+    this.panel.classList.add('panel-open');
+  })
+  .on('open', function() {
+    this.panel.addEventListener('click', close);
+  })
+  .on('beforeclose', function() {
+    this.panel.classList.remove('panel-open');
+    this.panel.removeEventListener('click', close);
+  });
